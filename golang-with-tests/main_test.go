9c86d12a9d7e1c6b36e06f77a8c98a1703d79293
ddanielsantos <contatodanieljob@gmail.com>
2022-05-20 00:45:51 -0300

    test: add subtest

--- golang-with-tests/main_test.go
+++ golang-with-tests/main_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Chris")
-	expected := "Hello, Chris!"
-
-	if result != expected {
-		t.Errorf("Result: '%s' Expected: '%s'", result, expected)
+	verifyCorrectString := func(t *testing.T, result, expected string) { // functions are first class citizens in Go
+		t.Helper() // especify that the t.Helper() is a helper function for the current test
+		if result != expected {
+			t.Errorf("Result: '%s' Expected: '%s'", result, expected)
+		}
 	}
+
+	t.Run("say hello to a pearson", func(t *testing.T) {
+		result := Hello("Chris")
+		expected := "Hello, Chris!"
+
+		verifyCorrectString(t, result, expected)
+	})
+
+	t.Run("say Hello, World when a empty string is supplied", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, World!"
+
+		verifyCorrectString(t, result, expected)
+	})
 }
